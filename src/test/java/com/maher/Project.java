7f60eb6ffb409e80af37e0d69945ee534b5857b2
maher <mansourmaher77@gmail.com>
2024-12-11 09:44:53 +0100

    first commit

---
+++ src/test/java/com/maher/Project.java
@@ -0,0 +1,15 @@
+package com.maher;
+
+import java.util.Date;
+
+public class Project {
+
+    public String id;
+    public String libelle;
+    public Date dateDebut;
+    public Date dateFin;
+    public float coast;
+    public int totlaTasks;
+
+    
+}
---
+++ src/test/java/com/maher/Team.java
@@ -0,0 +1,5 @@
+package com.maher;
+
+public class Team {
+    
+}
